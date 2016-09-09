3b6586628bd4b474633c9cf50241954a26a2879f
MisuzuShimakata <shimakata.misuzu@gmail.com>
2016-09-09 11:29:39 +0000

    Search and Toggle Menus

--- js/common.js
+++ js/common.js
@@ -1,5 +1,17 @@
 $(document).ready(function(){
-  $(".title").click(function(){
+  $(".humberger").click(function(){
     $(this).next().slideToggle();
+      if($(this).find('img.toggleicon').attr('src') == "./images/top/next_icon.png"){
+        $(this).find('img.toggleicon').attr('src','./images/top/down_icon.png');
+      }else{
+        $(this).find('img.toggleicon').attr('src','./images/top/next_icon.png');
+      }
+  });
+
+  $('.owl-carousel').owlCarousel({
+      loop:true,
+      nav:false,
+      items:1,
+      autoplay:true
   });
 });
