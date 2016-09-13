a18f9f5ba8d0f97f6a104374bf08e1a5747068c7
MisuzuShimakata <shimakata.misuzu@gmail.com>
2016-09-13 06:43:07 +0000

    ‘パンくず変更’

--- js/common.js
+++ js/common.js
@@ -1,11 +1,11 @@
 $(document).ready(function(){
   $(".humberger").click(function(){
+    if($(this).find('img.toggleicon').attr('src') == "./images/top/next_icon.png"){
+      $(this).find('img.toggleicon').attr('src','./images/top/down_icon.png');
+    }else{
+      $(this).find('img.toggleicon').attr('src','./images/top/next_icon.png');
+    }
     $(this).next().slideToggle();
-      if($(this).find('img.toggleicon').attr('src') == "./images/top/next_icon.png"){
-        $(this).find('img.toggleicon').attr('src','./images/top/down_icon.png');
-      }else{
-        $(this).find('img.toggleicon').attr('src','./images/top/next_icon.png');
-      }
   });
 
   $('.owl-carousel').owlCarousel({
@@ -14,4 +14,9 @@ $(document).ready(function(){
       items:1,
       autoplay:true
   });
+  
+  $(".thumbs li img").click(function(){
+    var img_src = $(this).attr("src");
+    $(".main-img").attr("src", img_src);
+  });  
 });
